68307933d1045c2cb1d6f066b9adc3cbffc37440
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2025-04-26 18:22:33 +0100

    component

---
+++ app/components/common/Button.tsx
@@ -0,0 +1,7 @@
+export default function Button({ children, url, classes, ...rest }: Properties) {
+    return (
+        <button className="">
+            {children}
+        </button>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <main className="px-5 pb-5 flex justify-center items-end relative z-10">
+        <main className="h-[calc(100vh-64px)] px-5 pb-5 flex justify-center items-end relative z-10">
             <h1 className="text-white text-7xl leading-23 font-semibold">Who needs<br/>Slash Commands<br/>when you have<br/>Agentic <span className="grad-text">AI</span>?</h1>
 
             <nav className="flex gap-4">
