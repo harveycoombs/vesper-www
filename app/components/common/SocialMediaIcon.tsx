7ae7e1751df712b6ea46ba450311da2c4428d5fe
harveycoombs <harveycoombs23@outlook.com>
2025-04-26 21:48:04 +0100

    Various

---
+++ app/components/common/SocialMediaIcon.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+interface Properties {
+    url: string;
+    icon: any;
+}
+
+export default function SocialMediaIcon({ url, icon }: Properties) {
+    return <Link href={url} target="_blank" rel="noopener" className="text-xl leading-none cursor-pointer select-none duration-200 hover:text-gray-400 active:text-gray-500"><FontAwesomeIcon icon={icon} /></Link>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChain } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faXTwitter, faDiscord } from "@fortawesome/free-brands-svg-icons";
 
 import Button from "@/app/components/common/Button";
+import SocialMediaIcon from "@/app/components/common/SocialMediaIcon";
 
 export default function Home() {
     return (
         <main className="w-full h-[calc(100vh-64px)] px-5 pb-5 flex justify-between items-end relative z-10">
             <h1 className="text-white text-8xl leading-30 font-semibold">Who needs<br/>Slash Commands<br/>when you have<br/>Agentic <span className="grad-text">AI</span>?</h1>
 
-            <nav className="flex gap-4">
+            <nav className="flex items-center gap-5">
                 <Button url="/"><FontAwesomeIcon icon={faChain} /> Invite Bot</Button>
+                <SocialMediaIcon url="https://github.com/harveycoombs" icon={faGithub} />
+                <SocialMediaIcon url="https://x.com/harveycoombs" icon={faXTwitter} />
+                <SocialMediaIcon url="https://discord.gg/vesper" icon={faDiscord} />
             </nav>
         </main>
     );
