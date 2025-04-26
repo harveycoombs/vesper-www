c5c4b558defade948790d3e2ff6bddaf85dea07b
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2025-04-26 19:01:01 +0100

    button component structure

--- app/components/common/Button.tsx
+++ app/components/common/Button.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
+interface Properties {
+    children: React.ReactNode;
+    url?: string;
+    classes?: string;
+    [key: string]: any;
+}
+
 export default function Button({ children, url, classes, ...rest }: Properties) {
-    return (
-        <button className="">
-            {children}
-        </button>
-    );
+    const classList = ` ${classes?.length ? classes : ""}`;
+    return url?.length ? <Link href={url} className={classList} {...rest}>{children}</Link> : <button className={classList} {...rest}>{children}</button>;
 }
