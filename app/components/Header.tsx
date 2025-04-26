dee8643d5f8afd893d0c9a3dcf8601eb1df89827
harveycoombs <harveycoombs23@outlook.com>
2025-04-26 22:31:08 +0100

    heeader media query

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="p-5 flex justify-between items-center">
             <strong className="text-white select-none font-bold">Vesper.gg</strong>
-            <div className="font-medium select-none text-sm leading-none">Created by <Link href="https://harveycoombs.com" target="_blank" rel="noopener" className="hover:underline">Harvey Coombs</Link></div>
+            <div className="font-medium select-none text-sm leading-none">2021 &ndash; {new Date().getFullYear()} &middot; <span className="max-sm:hidden">Created by </span><Link href="https://harveycoombs.com" target="_blank" rel="noopener" className="hover:underline">Harvey Coombs</Link></div>
         </header>
     );
 }
