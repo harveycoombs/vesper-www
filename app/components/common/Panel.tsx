2987a6e8255e43cf62f4b0f2e86b7612bb19cf04
harveycoombs <harveycoombs23@outlook.com>
2025-04-27 19:36:19 +0100

    panel component

---
+++ app/components/common/Panel.tsx
@@ -0,0 +1,8 @@
+export default function Panel({ children, title = "", classes = "", ...rest }: any) {
+    return (
+        <div className={`p-5 rounded-xl bg-[#131B29] ${classes.length ? classes : ""}`} {...rest}>
+            {title && <h2 className="block w-fit text-lg leading-none text-white font-bold">{title}</h2>}
+            {children}
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import { motion } from "motion/react";
 
+import Panel from "@/app/components/common/Panel";
+import Button from "@/app/components/common/Button";
+
 export default function Home() {
     return (
-        <main className="w-375 mx-auto">
-            <section className="flex items-center gap-30">
+        <main className="w-375 h-[calc(100vh-78px)] mx-auto grid place-items-center">
+            <section className="w-full flex items-center gap-30">
                 <div className="w-10/16 mb-5">
                     <h1 className="text-white text-[4rem] leading-22 font-semibold max-2xl:text-7xl max-2xl:leading-24 max-sm:text-6xl max-sm:leading-20">
                         <div className="overflow-hidden"><motion.span initial={{ y: "100%" }} animate={{ y: 0 }} transition={{ duration: 0.6 }} className="block">Who needs slash</motion.span></div>
@@ -12,7 +15,7 @@ export default function Home() {
                         <div className="overflow-hidden"><motion.span initial={{ y: "100%" }} animate={{ y: 0 }} transition={{ duration: 0.6, delay: 0.2 }} className="block">have Agentic <span className="grad-text">AI</span>?</motion.span></div>
                     </h1>
 
-                    <div className="mt-6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam eum voluptatum, necessitatibus fuga similique libero, omnis, vel molestias molestiae ut fugit repudiandae.</div>
+                    <div className="mt-6 leading-loose">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam eum voluptatum,<br/>necessitatibus fuga similique libero, omnis, vel molestias molestiae ut fugit repudiandae.</div>
                 </div>
 
                 <div className="w-6/16">
@@ -20,6 +23,20 @@ export default function Home() {
                     <HeroImage url="/images/hero.png" delay={0.1} x="8.3333%" classes="mb-8" />
                     <HeroImage url="/images/hero.png" delay={0.2} />
                 </div>
+            </section>
+
+            <section className="w-full h-76 flex gap-10">
+                <Panel title="Streamline & Simplify" classes="w-1/2 relative">
+                    <p className="block my-3.5 text-sm font-medium leading-relaxed">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit error molestias hic, ipsum sapiente aperiam blanditiis asperiores quae aliquid commodi ea praesentium exercitationem cupiditate repellendus ut quis magni voluptatibus omnis! Corporis, aut.</p>
+                    <Button url="/about" classes="block w-fit">Learn More</Button>
+                </Panel>
+
+                <Panel title="Join Our Discord" classes="w-1/2 relative">
+                    <div className="w-1/2 inline-block">
+                        <p className="block my-3.5 text-sm font-medium leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat repudiandae quis assumenda.</p>
+                        <Button url="#" classes="block w-fit" background="#5865F5">Join Server</Button>
+                    </div>
+                </Panel>
             </section>
         </main>
     );
