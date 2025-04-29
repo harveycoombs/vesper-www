7d6c5f1995c1175933e1f320ffcf87ccadf43414
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2025-04-29 13:11:05 +0100

    server count

--- lib/counter.ts
+++ lib/counter.ts
@@ -3,7 +3,7 @@ import pool from "./database";
 
 export async function getServerCount(): Promise<number> {
 	const [result]: any = await pool.query("SELECT server_count FROM bot_stats");
-	return result[0]?.server_count;
+	return result[0]?.server_count ?? 0;
 }
 
 export async function updateServerCount(count: number): Promise<void> {
