38a96d18428a481eb2e6e878af00f2f111311013
harveycoombs <harveycoombs23@outlook.com>
2025-05-03 17:47:39 +0100

    simplified counter api route

--- app/api/counter/route.ts
+++ app/api/counter/route.ts
@@ -1,22 +1,8 @@
 import { NextResponse } from "next/server";
 
-import { getServerCount, updateServerCount } from "@/lib/counter";
+import getServerCount from "@/lib/counter";
 
 export async function GET(_: Request): Promise<NextResponse> {
     const count = await getServerCount();
     return NextResponse.json({ count });
-}
-
-export async function POST(request: Request): Promise<NextResponse> {
-    try {
-        const { count, token } = await request.json();
-
-        if (!token?.length) return NextResponse.json({ error: "Unauthorised." }, { status: 401 });
-    
-        await updateServerCount(count);
-    
-        return NextResponse.json({ message: "Updated." });
-    } catch (ex: any) {
-        return NextResponse.json({ message: ex.message }, { status: 500 });
-    }
 }
