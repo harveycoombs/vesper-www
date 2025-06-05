da6ba5de81f9296a31f39c67ddb3343d510cd740
Harvey Coombs <harveycoombs23@outlook.com>
2025-06-05 17:08:16 +0100

    Updated content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,20 +10,14 @@ export default function AboutPage() {
                     <h2 className="text-4xl font-semibold text-white max-sm:text-3xl">What can I do with Vesper?</h2>
 
                     <p className="pt-3 leading-7 max-sm:text-sm">Vesper can manage just about any aspect of your server. From moderation to server customisation, Vesper makes the perfect moderator or administrator.</p>
-
-                    <p className="pt-3 leading-7 max-sm:text-sm">In addition to these agentic capabilites, Vesper also behaves as a regular chat bot, answering general questions and participating in conversations (optional).</p>
                 </article>
 
                 <article className="py-6">
                     <h2 className="text-4xl font-semibold text-white max-sm:text-3xl">How does Vepser work?</h2>
 
-                    <p className="pt-3 leading-7 max-sm:text-sm">There are two answers to this question.</p>
-
-                    <p className="pt-3 leading-7 max-sm:text-sm">Technically speaking, Vesper was written in Python, interacting with the Discord API directly (no 3rd-party wrappers). It currently leverages the Gemma 3 model for its capabilities.</p>
+                    <p className="pt-3 leading-7 max-sm:text-sm">Vesper was written in Python, interacting with the Discord API directly (no 3rd-party wrappers). It currently leverages the Gemma 3 model for its capabilities.</p>
 
                     <p className="pt-3 leading-7 max-sm:text-sm">As for how to use Vesper, simply ask it to do something (examples: 'Give me the Green role', 'Create a channel called gaming and make it private to the gaming role'), and it will oblige. Vesper can interpret many different writing styles, and is able to understand and respond to all kinds of requests.</p>
-
-                    <p className="pt-3 leading-7 max-sm:text-sm">Pretty cool, right?</p>
                 </article>
                 
                 <article className="py-6">
@@ -37,7 +31,7 @@ export default function AboutPage() {
                 <article className="py-6">
                     <h2 className="text-4xl font-semibold text-white max-sm:text-3xl">Is Vesper free?</h2>
 
-                    <p className="pt-3 leading-7 max-sm:text-sm">Vesper is currently in development. It is free to use up to 200 interactions per month, per server. Subscription plans to increase this limit, alongside other unlocking features, will be available soon.</p>
+                    <p className="pt-3 leading-7 max-sm:text-sm">Vesper is free to use up to 200 interactions per month, per server. Subscription plans to increase this limit, alongside other unlocking features, will be available soon.</p>
                 </article>                
             </section>
         </main>
