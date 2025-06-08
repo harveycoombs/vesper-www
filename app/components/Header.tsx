c9904b84b25e84a1ca501801e82df3cc1c6c788c
Harvey Coombs <harveycoombs23@outlook.com>
2025-06-08 15:37:45 +0100

    swipe animation for feature rows

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,16 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faLink } from "@fortawesome/free-solid-svg-icons";
 import { faDiscord } from "@fortawesome/free-brands-svg-icons";
 
 import Logo from "@/app/components/common/Logo";
 import Button from "@/app/components/common/Button";
 
 export default function Header() {
+    const [mobileMenuIsOpen, setMobileMenuOpen] = useState<boolean>(false);
+
     return (
         <header className="py-5 w-250 mx-auto flex items-center justify-between sticky top-0 max-lg:w-full max-lg:px-5">
             <nav className="flex items-center gap-8">
@@ -18,13 +22,19 @@ export default function Header() {
             </nav>
 
             <div>
-                <Button url="https://discord.com/oauth2/authorize?client_id=1028726248861605999&permissions=8&integration_type=0&scope=applications.commands+bot" target="_blank" rel="noopener noreferrer" classes="inline-block align-middle"><FontAwesomeIcon icon={faLink} className="text-rose-300 mr-1.5" />Invite</Button>
-                <Link href="/" className="inline-block align-middle ml-4.5 text-xl leading-none text-zinc-600 select-none cursor-pointer duration-200 hover:text-white"><FontAwesomeIcon icon={faDiscord} /></Link>
-            </div>
+                <HeaderIcon icon={faBars} onClick={() => setMobileMenuOpen(true)} />
+                <Button url="https://discord.com/oauth2/authorize?client_id=1028726248861605999&permissions=8&integration_type=0&scope=applications.commands+bot" target="_blank" rel="noopener noreferrer" classes="inline-block align-middle mx-4.5"><FontAwesomeIcon icon={faLink} className="text-rose-300 mr-1.5" />Invite</Button>
+                <HeaderIcon url="https://discord.gg/aF4EctqgPS" icon={faDiscord} />
+            </div>  
         </header>
     );
 }
 
 function HeaderLink({ url, children }: any) {
     return <Link href={url} className="text-sm font-medium select-none cursor-pointer duration-200 hover:text-white">{children}</Link>;
+}
+
+function HeaderIcon({ url = "", icon, ...rest }: any) {
+    const classList = "inline-block align-middle text-xl leading-none text-zinc-600 select-none cursor-pointer duration-200 hover:text-white";  
+    return url.length ? <Link href={url} className={classList} {...rest}><FontAwesomeIcon icon={icon} /></Link> : <button className={classList} {...rest}><FontAwesomeIcon icon={icon} /></button>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,33 +5,33 @@ import Button from "@/app/components/common/Button";
 
 export default function Home() {
     return (
-        <main className="w-250 min-h-[calc(100vh-138px)] mx-auto grid place-items-center max-lg:w-full max-lg:px-5">
-            <section>
-                <h1 className="text-white text-6xl text-center leading-23 font-semibold">
+        <main className="w-250 min-h-[calc(100vh-138px)] mx-auto grid place-items-center max-lg:w-full max-lg:px-5 max-md:block">
+            <section className="max-md:pb-6">
+                <h1 className="text-white text-6xl text-center leading-23 font-semibold max-lg:text-5xl max-lg:leading-20 max-md:text-4xl max-md:leading-16">
                     <div className="overflow-hidden"><motion.span initial={{ y: "100%" }} animate={{ y: 0 }} transition={{ duration: 0.5 }} className="block">Who needs slash commands</motion.span></div>
                     <div className="overflow-hidden"><motion.span initial={{ y: "100%" }} animate={{ y: 0 }} transition={{ duration: 0.5, delay: 0.25 }} className="block">when you have <span className="text-rose-500">Agentic AI</span>?</motion.span></div>
                 </h1>
 
-                <motion.div className="mt-12" initial={{ opacity: 0, y: 35 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.4, delay: 0.7 }}>
-                    <p className="text-lg text-center block">Vesper is an agentic AI Discord bot designed with effortless automation in mind.</p>
+                <motion.div className="mt-12 max-lg:mt-8 max-md:mt-4.5" initial={{ opacity: 0, y: 35 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.4, delay: 0.7 }}>
+                    <p className="text-lg text-center block max-lg:text-base">Vesper is an agentic AI Discord bot designed with effortless automation in mind.</p>
 
-                    <div className="w-fit mx-auto mt-14">
+                    <div className="w-fit mx-auto mt-14 max-lg:mt-12 max-md:mt-8">
                         <Button url="https://discord.com/oauth2/authorize?client_id=1028726248861605999&permissions=8&integration_type=0&scope=applications.commands+bot" classes="inline-block align-middle w-32">Get Started</Button>
                         <Button url="/about" classes="inline-block align-middle w-32 ml-4" alt={true}>Learn More</Button>
                     </div>
                 </motion.div>
             </section>
 
-            <section>
-                <FeatureRow title="Server Management" image="/images/home/server-management.png">
+            <section className="overflow-hidden">
+                <FeatureRow title="Server Management" image="/images/home/server-management.png" delay={1.1}>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus dignissimos quisquam in aliquam ratione hic sed, asperiores aspernatur adipisci est magni aliquid doloremque, autem nulla esse.
                 </FeatureRow>
 
-                <FeatureRow title="Moderation" image="/images/home/moderation.png" reverse={true}>
+                <FeatureRow title="Moderation" image="/images/home/moderation.png" delay={1.3} reverse={true}>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus dignissimos quisquam in aliquam ratione hic sed, asperiores aspernatur adipisci est magni aliquid doloremque, autem nulla esse.
                 </FeatureRow>
 
-                <FeatureRow title="And So Much More" image="/images/home/more.png">
+                <FeatureRow title="And So Much More" image="/images/home/more.png" delay={1.5}>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus dignissimos quisquam in aliquam ratione hic sed, asperiores aspernatur adipisci est magni aliquid doloremque, autem nulla esse.
                 </FeatureRow>             
             </section>
@@ -39,17 +39,27 @@ export default function Home() {
     );
 }
 
-function FeatureRow({ title, children, image, reverse = false }: any) {
+function FeatureRow({ title, children, image, delay = 0, reverse = false }: any) {
     return (
-        <article className={`flex items-center gap-10 ${reverse ? "flex-row-reverse" : ""} mb-12 select-none`}>
-            <div className="w-1/2">
+        <motion.article 
+            className={`flex items-center gap-10 ${reverse ? "flex-row-reverse" : ""} mb-12 select-none max-lg:mb-6 max-lg:gap-6 max-md:flex-col`}
+            initial={{ x: reverse ? "-100%" : "100%" }}
+            animate={{ x: 0 }}
+            transition={{ 
+                type: "spring", 
+                stiffness: 100, 
+                damping: 12,
+                delay: delay
+            }}
+        >
+            <div className="w-1/2 max-md:w-full">
                 <h2 className="block mb-2.25 text-xl font-semibold text-white">{title}</h2>
                 <p className="text-sm leading-6">{children}</p>
             </div>
 
-            <div className="w-1/2">
+            <div className="w-1/2 max-md:w-full">
                 <img src={image} alt={title} className="w-full h-auto rounded-lg aspect-[60/17] object-cover" />
             </div>
-        </article>        
+        </motion.article>
     );
 }
