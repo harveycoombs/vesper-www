b95a3107a9ad6c9b9ef1b49f1d7265ed3937fc55
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2025-07-11 12:57:52 +0100

    Updated model

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -93,8 +93,8 @@ export default function Manage() {
                     <Panel title="Bot Settings" classes="w-1/2" loading={settingsLoading}>
                         {settingsError.length > 0 && <div className="text-red-500 font-medium text-sm mb-2">{settingsError}</div>}
                         <FieldContainer title="Model">
-                            <Menu defaultValue="magistral-small-2506" disabled title="Temporarily Unavailable">
-                                <option value="magistral-small-2506">Magistral-Small-2506</option>
+                            <Menu defaultValue="gemma-3-12b" disabled title="Temporarily Unavailable">
+                                <option value="gemma-3-12b">Gemma 3 12B</option>
                             </Menu>
                         </FieldContainer>
                     </Panel>
