425083aa56a5ca2f3305f7b3b5ea8707cb435aa7
Harvey Coombs <harveycoombs23@outlook.com>
2025-07-12 15:07:50 +0100

    Added exception handling to guild count store

--- app/api/guilds/total/route.ts
+++ app/api/guilds/total/route.ts
@@ -2,12 +2,26 @@ import { NextResponse } from "next/server";
 import { createClient } from "redis";
 
 export async function GET(_: Request) {
-    const client = createClient({
-        url: process.env.REDIS_URL ?? ""
-    }); 
+    try {
+        const client = createClient({
+            url: process.env.REDIS_URL ?? "",
+            socket: {
+                connectTimeout: 15000,
+                keepAlive: true,
+                reconnectStrategy: (retries) => Math.min(retries * 50, 1000)
+            }
+        }); 
 
-    await client.connect();
-    const total = await client.get("guild_count") ?? "0";
+        client.on("error", (err) => console.error('Redis Client Error', err));
 
-    return NextResponse.json({ total: parseInt(total) });
+        await client.connect();
+        const total = await client.get("guild_count") ?? "0";
+
+        await client.quit();
+
+        return NextResponse.json({ total: parseInt(total) });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: "Failed to fetch guild count" }, { status: 500 });
+    }
 }
